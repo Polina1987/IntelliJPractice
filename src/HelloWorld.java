2c29036de89e4cc5d11fbc81b519faa39efe4856
Polina Blintsovskaya <polina.blintsovskaya@agilethought.com>
2022-09-04 17:02:38 -0400

    Test master branch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        //Use IntelliJ practice to review Git basics
         System.out.println("I am learning how to use IntelliJ IDE, and I think it is better than Eclipse IDE.");
     }
 }
