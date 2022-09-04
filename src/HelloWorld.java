aa1bbf138ec1c0e62578453d150e703e2ebd9b2c
Polina Blintsovskaya <polina.blintsovskaya@agilethought.com>
2022-09-04 16:10:45 -0400

    Pushing HelloWorld to GitHub repo

---
+++ src/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("I am learning how to use IntelliJ IDE, and I think it is better than Eclipse IDE.");
+    }
+}
